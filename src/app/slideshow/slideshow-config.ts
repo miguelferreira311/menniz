8b8325a80a06380cd1d910c88b6f58c0e9b73161
Miguel Laires Ferreira <miguel.laires.ferreira@finpay.pt>
2024-01-24 10:30:44 +0000

    Updated images.

--- src/app/slideshow/slideshow-config.ts
+++ src/app/slideshow/slideshow-config.ts
@@ -2,70 +2,50 @@ import {Projects} from "../constants-types";
 
 export const IMAGES_BY_PROJECT = {
   [Projects.JOAO_BRANCO]: [
-    'assets/images/01_Joao_Branco/00_01_Joao_Branco.jpeg',
-    'assets/images/01_Joao_Branco/01_Photoshoot1V2A4317-HDR.jpg',
-    'assets/images/01_Joao_Branco/Drone (2).jpg',
-    'assets/images/01_Joao_Branco/Drone (4).jpg',
-    'assets/images/01_Joao_Branco/Photoshoot (3).jpg',
-    'assets/images/01_Joao_Branco/Photoshoot (4).jpg',
-    'assets/images/01_Joao_Branco/Photoshoot (6).jpg',
-    'assets/images/01_Joao_Branco/Photoshoot (8).jpg',
-    'assets/images/01_Joao_Branco/Photoshoot (9).jpg',
-    'assets/images/01_Joao_Branco/Photoshoot (10).jpg',
-    'assets/images/01_Joao_Branco/Photoshoot (11).jpg',
-    'assets/images/01_Joao_Branco/Photoshoot (12).jpg',
-    'assets/images/01_Joao_Branco/Photoshoot (13).jpg',
-    'assets/images/01_Joao_Branco/Photoshoot (15).jpg',
-    'assets/images/01_Joao_Branco/Photoshoot (21).jpg',
-    'assets/images/01_Joao_Branco/Photoshoot (22).jpg',
-    'assets/images/01_Joao_Branco/Photoshoot (23).jpg',
-    'assets/images/01_Joao_Branco/Photoshoot (25).jpg',
-    'assets/images/01_Joao_Branco/Photoshoot (26).jpg',
-    'assets/images/01_Joao_Branco/Photoshoot 1V2A4844-HDR-2.jpg',
-    'assets/images/01_Joao_Branco/Photoshoot 1V2A5987-HDR_versao cortada.png',
-    'assets/images/01_Joao_Branco/xx.jpg',
-    'assets/images/01_Joao_Branco/yy.jpg',
-    'assets/images/01_Joao_Branco/zz.jpg'
+    'assets/images/01_Joao_Branco/00_Photoshoot (21).jpg',
+    'assets/images/01_Joao_Branco/01_Photoshoot (4).jpg',
+    'assets/images/01_Joao_Branco/02_Photoshoot (11).jpg',
+    'assets/images/01_Joao_Branco/03_1V2A5711.jpg',
+    'assets/images/01_Joao_Branco/04_Photoshoot (8)_ajustada1.jpg',
+    'assets/images/01_Joao_Branco/05_Drone (4)_ajustada1.jpg',
+    'assets/images/01_Joao_Branco/06_Photoshoot (22)_ajustada.jpg',
+    'assets/images/01_Joao_Branco/07_Photoshoot (6)_ajustada4.jpg',
+    'assets/images/01_Joao_Branco/08_Photoshoot (3).jpg',
+    'assets/images/01_Joao_Branco/09_zz.jpg',
+    'assets/images/01_Joao_Branco/10_yy_ajustado.jpg',
+    'assets/images/01_Joao_Branco/11_Photoshoot 1V2A5987-HDR_versao cortada.png',
+    'assets/images/01_Joao_Branco/12_Drone (2).jpg'
   ],
   [Projects.ABRIL_25]: [
-    'assets/images/02_25 de Abril/00_teste.jpeg',
-    'assets/images/02_25 de Abril/1_1V2A6259-Pano-Pano-Editar.jpg',
-    'assets/images/02_25 de Abril/1V2A6063-Editar.jpg',
-    'assets/images/02_25 de Abril/1V2A6104.jpg',
-    'assets/images/02_25 de Abril/1V2A6136.jpg',
-    'assets/images/02_25 de Abril/1V2A6162.jpg',
-    'assets/images/02_25 de Abril/1V2A6189.jpg',
-    'assets/images/02_25 de Abril/1V2A6209.jpg',
-    'assets/images/02_25 de Abril/1V2A6368.jpg',
-    'assets/images/02_25 de Abril/1V2A6374-Pano-Editar-2.jpg',
-    'assets/images/02_25 de Abril/1V2A6435-HDR.jpg',
-    'assets/images/02_25 de Abril/1V2A6465-Pano.jpg',
-    'assets/images/02_25 de Abril/1V2A6470.jpg',
-    'assets/images/02_25 de Abril/1V2A6481-Editar.jpg',
-    'assets/images/02_25 de Abril/1V2A6484.jpg',
-    'assets/images/02_25 de Abril/1V2A6496.jpg',
-    'assets/images/02_25 de Abril/1V2A6507.jpg',
-    'assets/images/02_25 de Abril/1V2A6633-HDR.jpg'
+    'assets/images/02_25 de Abril/00_1V2A6224.jpg',
+    'assets/images/02_25 de Abril/01_1V2A6302-Pano.jpg',
+    'assets/images/02_25 de Abril/02_1V2A6465-Pano.jpg',
+    'assets/images/02_25 de Abril/03_1V2A6374-Pano-Editar-2.jpg',
+    'assets/images/02_25 de Abril/04_1V2A6162.jpg',
+    'assets/images/02_25 de Abril/05_1V2A6063-Editar_ajustada.jpg',
+    'assets/images/02_25 de Abril/06_1V2A6470.jpg'
   ],
   [Projects.MIRANTE]: [
     'assets/images/03_Mirante/00_1V2A5596-Panorノica HDR.jpg',
     'assets/images/03_Mirante/01_1V2A5534-HDR.jpg',
     'assets/images/03_Mirante/02_1V2A5580-Panorノica HDR.jpg',
-    'assets/images/03_Mirante/04_1V2A5707-Editar.jpg',
-    'assets/images/03_Mirante/05_1V2A5755.jpg'
+    'assets/images/03_Mirante/03_1V2A5707-Editar.jpg',
+    'assets/images/03_Mirante/04_1V2A5755.jpg'
 
   ],
   [Projects.FREITAS_REIS]: [
     'assets/images/04_Freitas Reis/00_1V2A0107-Pano.jpg',
     'assets/images/04_Freitas Reis/01_1V2A0121-Pano.jpg',
-    'assets/images/04_Freitas Reis/02_1V2A0287.jpg',
-    'assets/images/04_Freitas Reis/03_1V2A9951-P.jpg'
+    'assets/images/04_Freitas Reis/02_IMG_9894_ajustada v3.jpg',
+    'assets/images/04_Freitas Reis/03_1V2A0194-Editar_pormenor 3_ajustada.jpg',
+    'assets/images/04_Freitas Reis/04_IMG_9891_ajustada v2.jpg'
   ],
   [Projects.INFANTE_SAGRES]: [
     'assets/images/05_Infante Sagres/00_1V2A8029-Pano-Editar.jpg',
     'assets/images/05_Infante Sagres/01_1V2A8018.jpg',
     'assets/images/05_Infante Sagres/02_1V2A8036-Editar.jpg',
     'assets/images/05_Infante Sagres/03_1V2A8081.jpg',
-    'assets/images/05_Infante Sagres/04_1V2A8293.jpg'
+    'assets/images/05_Infante Sagres/04_X_1V2A8072.jpg',
+    'assets/images/05_Infante Sagres/05_1V2A8293.jpg'
   ]
 }
