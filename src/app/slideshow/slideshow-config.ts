fb5d490ca6a0e860066af1c7c711651c3c553a98
Miguel Laires Ferreira <miguel.laires.ferreira@finpay.pt>
2024-02-05 22:33:50 +0000

    Change fonts and arrow svg. Change back the images to bigger size. Fix about animation background.

--- src/app/slideshow/slideshow-config.ts
+++ src/app/slideshow/slideshow-config.ts
@@ -2,47 +2,47 @@ import {Projects} from "../constants-types";
 
 export const IMAGES_BY_PROJECT = {
   [Projects.JOAO_BRANCO]: [
-    'assets/images/01_Joao_Branco/00_Photoshoot (21).jpg',
-    'assets/images/01_Joao_Branco/01_Photoshoot (4).jpg',
-    'assets/images/01_Joao_Branco/02_Photoshoot (11).jpg',
-    'assets/images/01_Joao_Branco/03_1V2A5711.jpg',
-    'assets/images/01_Joao_Branco/04_Photoshoot (8)_ajustada1.jpg',
-    'assets/images/01_Joao_Branco/05_Drone (4)_ajustada1.jpg',
-    'assets/images/01_Joao_Branco/06_Photoshoot (6)_ajustada4.jpg',
-    'assets/images/01_Joao_Branco/07_Photoshoot (22)_ajustada.jpg',
-    'assets/images/01_Joao_Branco/08_Photoshoot (3).jpg',
-    'assets/images/01_Joao_Branco/09_zz.jpg',
-    'assets/images/01_Joao_Branco/10_Photoshoot 1V2A5987-HDR_versao cortada.png',
-    'assets/images/01_Joao_Branco/11_Drone (2).jpg'
+    'assets/images/01_João Branco/00_Photoshoot (21).jpg',
+    'assets/images/01_João Branco/01_Photoshoot (4).jpg',
+    'assets/images/01_João Branco/02_Photoshoot (11).jpg',
+    'assets/images/01_João Branco/03_1V2A5711.jpg',
+    'assets/images/01_João Branco/04_Photoshoot (8)_ajustada1.jpg',
+    'assets/images/01_João Branco/05_Drone (4)_ajustada1.jpg',
+    'assets/images/01_João Branco/06_Photoshoot (6)_ajustada4.jpg',
+    'assets/images/01_João Branco/07_Photoshoot (22)_ajustada.jpg',
+    'assets/images/01_João Branco/08_Photoshoot (3).jpg',
+    'assets/images/01_João Branco/09_zz.jpg',
+    'assets/images/01_João Branco/10_Photoshoot 1V2A5987-HDR_versao cortada.png',
+    'assets/images/01_João Branco/11_Drone (2).jpg',
   ],
   [Projects.ABRIL_25]: [
-    'assets/images/02_25 de Abril/00_1V2A6224-Aprimorado-SR.jpg',
-    'assets/images/02_25 de Abril/02_1V2A6162-Aprimorado-SR.jpg',
-    'assets/images/02_25 de Abril/03_1V2A6465-Pano-Aprimorado-SR.jpg',
-    'assets/images/02_25 de Abril/04_1V2A6374-Pano-Editar-2-Aprimorado-SR.jpg',
-    'assets/images/02_25 de Abril/05_1V2A6063-Editar_ajustada-Aprimorado-SR.jpg',
-    'assets/images/02_25 de Abril/06_1V2A6470-Aprimorado-SR.jpg'
+    'assets/images/02_25 de Abril/00_1V2A6224.jpg',
+    'assets/images/02_25 de Abril/02_1V2A6162.jpg',
+    'assets/images/02_25 de Abril/03_1V2A6465-Pano.jpg',
+    'assets/images/02_25 de Abril/04_1V2A6374-Pano-Editar-2.jpg',
+    'assets/images/02_25 de Abril/05_1V2A6063-Editar_ajustada.jpg',
+    'assets/images/02_25 de Abril/06_1V2A6470.jpg'
   ],
   [Projects.MIRANTE]: [
-    'assets/images/03_Mirante/00_1V2A5596-Panorノica HDR-Aprimorado-SR.jpg',
-    'assets/images/03_Mirante/01_1V2A5534-HDR-Aprimorado-SR.jpg',
-    'assets/images/03_Mirante/02_1V2A5580-Panorノica HDR-Aprimorado-SR.jpg',
-    'assets/images/03_Mirante/03_1V2A5707-Editar-Aprimorado-SR.jpg',
-    'assets/images/03_Mirante/04_1V2A5755-Aprimorado-SR.jpg'
+    'assets/images/03_Mirante/00_1V2A5596-Panorノica HDR.jpg',
+    'assets/images/03_Mirante/01_1V2A5534-HDR.jpg',
+    'assets/images/03_Mirante/02_1V2A5580-Panorノica HDR.jpg',
+    'assets/images/03_Mirante/03_1V2A5707-Editar.jpg',
+    'assets/images/03_Mirante/04_1V2A5755.jpg'
 
   ],
   [Projects.FREITAS_REIS]: [
-    'assets/images/04_Freitas Reis/00_1V2A0107-Pano-Aprimorado-SR.jpg',
-    'assets/images/04_Freitas Reis/01_1V2A0121-Pano-Aprimorado-SR.jpg',
-    'assets/images/04_Freitas Reis/02_IMG_9894_ajustada v3-Aprimorado-SR.jpg',
-    'assets/images/04_Freitas Reis/03_1V2A0194-Editar_pormenor 3_ajustada-Aprimorado-SR.jpg',
-    'assets/images/04_Freitas Reis/04_IMG_9891_ajustada v2-Aprimorado-SR.jpg'
+    'assets/images/04_Freitas Reis/00_1V2A0107-Pano.jpg',
+    'assets/images/04_Freitas Reis/01_1V2A0121-Pano.jpg',
+    'assets/images/04_Freitas Reis/02_IMG_9894_ajustada v3.jpg',
+    'assets/images/04_Freitas Reis/03_1V2A0194-Editar_pormenor 3_ajustada.jpg',
+    'assets/images/04_Freitas Reis/04_IMG_9891_ajustada v2.jpg'
   ],
   [Projects.INFANTE_SAGRES]: [
-    'assets/images/05_Infante Sagres/00_1V2A8029-Pano-Editar-Aprimorado-SR.jpg',
-    'assets/images/05_Infante Sagres/02_1V2A8036-Editar-Aprimorado-SR.jpg',
-    'assets/images/05_Infante Sagres/03_1V2A8081-Aprimorado-SR.jpg',
-    'assets/images/05_Infante Sagres/04_X_1V2A8072-Aprimorado-SR.jpg',
-    'assets/images/05_Infante Sagres/05_1V2A8293-Aprimorado-SR.jpg'
+    'assets/images/05_Infante Sagres/00_1V2A8029-Pano-Editar.jpg',
+    'assets/images/05_Infante Sagres/02_1V2A8036-Editar.jpg',
+    'assets/images/05_Infante Sagres/03_1V2A8081.jpg',
+    'assets/images/05_Infante Sagres/04_X_1V2A8072.jpg',
+    'assets/images/05_Infante Sagres/05_1V2A8293.jpg'
   ]
 }
