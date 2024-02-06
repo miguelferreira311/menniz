9a0a753ae09a4646c9699a6b38d91a7aeb36fc40
Miguel Laires Ferreira <miguel.laires.ferreira@finpay.pt>
2024-02-06 00:02:54 +0000

    Add animation to scrolling description.

--- src/app/vertical-slideshow/vertical-slideshow.component.ts
+++ src/app/vertical-slideshow/vertical-slideshow.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, HostListener, Output} from '@angular/core';
 import {ProjectItem, PROJECTS} from "./projects-config";
 import {NgOptimizedImage} from "@angular/common";
 
@@ -13,10 +13,45 @@ import {NgOptimizedImage} from "@angular/common";
 })
 export class VerticalSlideshowComponent {
   @Output() public onProjectSelect: EventEmitter<ProjectItem> = new EventEmitter();
-
   public projects: ProjectItem[] = PROJECTS;
+  public activeDescriptionIndex: number = 0;
 
-  public selectProject(project: ProjectItem) {
+  @HostListener('window:scroll', ['$event'])
+  onScroll(_event: Event) {
+    this._changeVisibleDescription();
+  }
+
+  constructor(private el: ElementRef) {
+  }
+
+  public selectProject(project: ProjectItem): void {
     this.onProjectSelect.emit(project);
+  }
+
+  private _changeVisibleDescription(): void {
+    const windowHeight: number = window.innerHeight;
+    const componentOffset = this.el.nativeElement.offsetTop;
+
+    this.projects.forEach((_image: ProjectItem, index: number) => {
+      this._activateDescription(componentOffset, windowHeight, index);
+    });
+  }
+
+  private _activateDescription(componentOffset: number, windowHeight: number, index: number) {
+    const imageElement = this.el.nativeElement.querySelectorAll('.project-image')[index];
+    const rect = imageElement.getBoundingClientRect();
+
+    // Calculate the position of the top and bottom of the image relative to the viewport
+    const topPosition: number = rect.top - componentOffset;
+    const bottomPosition: number = rect.bottom - componentOffset;
+
+    if (topPosition < windowHeight * 0.5 && bottomPosition > windowHeight * 0.5) {
+      // Check if a new image is starting to appear
+      if (index !== this.activeDescriptionIndex) {
+
+        // Update the visibleImageIndex to avoid triggering the action multiple times
+        this.activeDescriptionIndex = index;
+      }
+    }
   }
 }
